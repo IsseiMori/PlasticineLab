3757fa0034c0817f81ab5b8f753ed8095ca29935
IsseiMori <ganntyu1225issei@gmail.com>
2022-03-30 22:25:33 -0700

    use taichi==0.7.26

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-install_requires = ['scipy', 'numpy', 'torch', 'opencv-python', 'tqdm', 'taichi', 'gym', 'tensorboard', 'yacs', 'baselines']
+install_requires = ['scipy', 'numpy', 'torch', 'opencv-python', 'tqdm', 'taichi==0.7.26', 'gym', 'tensorboard', 'yacs', 'baselines']
 
 setup(name='plb',
       version='0.0.1',
